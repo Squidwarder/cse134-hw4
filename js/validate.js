8ec51104e00a83e73ac8916f818546ffc3a282c4
SeaSponge <61109988+Squidwarder@users.noreply.github.com>
2023-11-25 12:49:39 -0800

    Name validate works

--- js/validate.js
+++ js/validate.js
@@ -10,7 +10,57 @@ const commentValidate = document.getElementById("form_vis_comment");
 const commentErrMsg = document.getElementById("comment_error");
 const commentInfoMsg = document.getElementById("comment_info_msg");
 
+document.addEventListener("DOMContentLoaded", function() {
+    if (nameValidate.validity.typeMismatch || nameValidate.validity.valueMissing) {
+        console.log("Invalid name");        
+        if (nameValidate.validity.typeMismatch) {
+            nameErrMsg.textContent = "Invalid Name";
+            nameInfoMsg.textContent = "Only UTF-8 characters are accepted";
+        } else {
+            nameErrMsg.textContent = "No input detected";
+            nameInfoMsg.textContent = "Please input a name it's required";
+        }
+    } else {        
+        nameErrMsg.textContent = "No errors detected";
+        nameInfoMsg.textContent = "Nice, this looks good";
+        console.log("Valid name");
+    }
+
+    if (emailValidate.validity.typeMismatch || emailValidate.validity.valueMissing) {
+        console.log("Invalid email");        
+        if (emailValidate.validity.typeMismatch) {
+            emailErrMsg.textContent = "Invalid email address";
+            emailInfoMsg.textContent = "Did you forget to put @?";
+        } else {
+            emailErrMsg.textContent = "No input detected";
+            emailInfoMsg.textContent = "please input an email, it's required";
+        }
+    } else {        
+        emailErrMsg.textContent = "No errors detected";
+        emailInfoMsg.textContent = "Nice, this looks good";
+        console.log("Valid email");
+    }
+});
+
 // ! DO NOT set custom validity. If done so, form wouldn't be updated on the fly
+nameValidate.addEventListener("input", (event) => {
+    console.log("Name validate() invoked")
+    if (nameValidate.validity.typeMismatch || nameValidate.validity.valueMissing) {
+        console.log("Invalid name");        
+        if (nameValidate.validity.typeMismatch) {
+            nameErrMsg.textContent = "Invalid Name";
+            nameInfoMsg.textContent = "Only UTF-8 characters are accepted";
+        } else {
+            nameErrMsg.textContent = "No input detected";
+            nameInfoMsg.textContent = "Please input a name it's required";
+        }
+    } else {        
+        nameErrMsg.textContent = "No errors detected";
+        nameInfoMsg.textContent = "Nice, this looks good";
+        console.log("Valid name");
+    }
+});
+
 emailValidate.addEventListener("input", (event) => {
     console.log("Email validate() invoked")
     if (emailValidate.validity.typeMismatch || emailValidate.validity.valueMissing) {
