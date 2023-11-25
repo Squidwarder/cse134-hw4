8701dc2ee9f0d8044b6512a935ad5ecacf8bf1ef
SeaSponge <61109988+Squidwarder@users.noreply.github.com>
2023-11-25 13:19:19 -0800

    used setCustomValidity correctly

--- js/validate.js
+++ js/validate.js
@@ -60,7 +60,8 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-// ! DO NOT set custom validity. If done so, form wouldn't be updated on the fly
+// ! Need to set CustomValidity to default in the case of no errors.
+// ! Otherwise the form display won't be updated on the fly
 nameValidate.addEventListener("input", (event) => {
     // console.log("Name validate() invoked");
     if (nameValidate.validity.typeMismatch || nameValidate.validity.valueMissing) {
@@ -68,13 +69,16 @@ nameValidate.addEventListener("input", (event) => {
         if (nameValidate.validity.typeMismatch) {
             nameErrMsg.textContent = "Invalid Name";
             nameInfoMsg.textContent = "Only UTF-8 characters are accepted";
+            nameValidate.setCustomValidity("Please use valid characters");
         } else {
             nameErrMsg.textContent = "No input detected";
             nameInfoMsg.textContent = "Please input a name it's required";
+            nameValidate.setCustomValidity("Please input a name");
         }
     } else {        
         nameErrMsg.textContent = "No errors detected";
         nameInfoMsg.textContent = "Nice, this looks good";
+        nameValidate.setCustomValidity("");
         // console.log("Valid name");
     }
 });
@@ -86,13 +90,16 @@ emailValidate.addEventListener("input", (event) => {
         if (emailValidate.validity.typeMismatch) {
             emailErrMsg.textContent = "Invalid email address";
             emailInfoMsg.textContent = "Did you forget to put @?";
+            emailValidate.setCustomValidity("Please input a valid email address");
         } else {
             emailErrMsg.textContent = "No input detected";
             emailInfoMsg.textContent = "please input an email, it's required";
+            emailValidate.setCustomValidity("Please input an email");
         }
     } else {        
         emailErrMsg.textContent = "No errors detected";
         emailInfoMsg.textContent = "Nice, this looks good";
+        emailValidate.setCustomValidity("");
         // console.log("Valid email");
     }
 });
@@ -104,16 +111,20 @@ commentValidate.addEventListener("input", (event) => {
         if (commentValidate.validity.typeMismatch) {
             commentErrMsg.textContent = "Invalid Comment";
             commentInfoMsg.textContent = "Only UTF-8 characters are accepted";
+            commnetValidate.setCustomValidity("Please use valid characters");
         } else if (commentValidate.validity.valueMissing) {
             commentErrMsg.textContent = "No input detected";
             commentInfoMsg.textContent = "Please input some comments, it's required";
+            commentValidate.setCustomValidity("Please input some comments");
         } else {
             commentErrMsg.textContent = "Comment too long";
             commentInfoMsg.textContent = "Please make it shorter, less than 200 characters";
+            commentValidate.setCustomValidity("Please make the message shorter");
         }
     } else {        
         commentErrMsg.textContent = "No errors detected";
         commentInfoMsg.textContent = (200 - commentValidate.value.length) + " Characters left";
+        commentValidate.setCustomValidity("");
         // console.log("Valid comment");
     }
 })
